3090e4282ae6b3a116f5c5fa37c8d4ce16c04c8f
Tony DiCola <tony@tonydicola.com>
2018-01-25 17:28:06 -0800

    Update example with alternate I2C address usage.

--- examples/simpletest.py
+++ examples/simpletest.py
@@ -1,5 +1,6 @@
 # Simple demo of setting the DAC value up and down through its entire range
 # of values.
+# Author: Tony DiCola
 import board
 import busio
 
@@ -11,6 +12,8 @@ i2c = busio.I2C(board.SCL, board.SDA)
 
 # Initialize MCP4725.
 dac = adafruit_mcp4725.MCP4725(i2c)
+# Optionally you can specify a different addres if you override the A0 pin.
+#amp = adafruit_max9744.MAX9744(i2c, address=0x63)
 
 # There are a three ways to set the DAC output, you can use any of these:
 dac.value = 65535  # Use the value property with a 16-bit number just like
